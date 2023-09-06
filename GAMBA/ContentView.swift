80f79a3318edce9c97ba586b357eeda5728c82be
Asher McConnell <asherryan7@gmail.com>
2023-09-06 16:20:11 -0600

    Initial Commit

---
+++ GAMBA/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  GAMBA
+//
+//  Created by Asher McConnell on 9/6/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GAMBA/GAMBAApp.swift
@@ -0,0 +1,17 @@
+//
+//  GAMBAApp.swift
+//  GAMBA
+//
+//  Created by Asher McConnell on 9/6/23.
+//
+
+import SwiftUI
+
+@main
+struct GAMBAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
